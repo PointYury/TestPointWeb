0dc244916a3d52b20a13abdf44604211198b6c80
Vitali Liadenka <phoenixsword6@gmail.com>
2023-12-11 01:58:57 +0300

    Fixed local storage

---
+++ assets/AuthenticationView-0d064852.js
@@ -0,0 +1,1 @@
+import{aa as e,V as t,Z as a,a5 as s,aq as c}from"./index-d799c111.js";const o={},n={class:"h-[100vh]"},r=c("div",{class:"relative h-full"},null,-1);function _(i,l){return t(),a("div",n,[s(" 12312312 "),r])}const f=e(o,[["render",_]]);export{f as default};
---
+++ assets/HomeView-7b8b5aca.js
@@ -0,0 +1,1 @@
+import{aa as e,V as t,Z as c}from"./index-d799c111.js";const s={},o={class:"q-pa-md q-gutter-sm"};function a(r,n){return t(),c("div",o)}const m=e(s,[["render",a]]);export{m as default};
